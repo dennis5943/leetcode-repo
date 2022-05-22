2451f49b4196d035565ce7c36cb767e24704ded7
Dennis <dennis@DennisdeMacBook-Pro.local>
2022-05-23 00:33:16 +0800

    0015

--- Python/S0015-3sum.py
+++ Python/S0015-3sum.py
@@ -8,6 +8,7 @@ The solution set must not contain duplicate triplets.
 Example:
 
 Given array nums = [-1, 0, 1, 2, -1, -4],
+-4 -1 -1 0 1 2
 
 A solution set is:
 [
@@ -15,33 +16,28 @@ A solution set is:
   [-1, -1, 2]
 ]
 """
-from typing import List
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
         nums.sort()
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i] != nums[i-1]:
-                self.twoSumII(nums, i, res)
-        return res
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i-1]:
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+        return list(res)
 
-    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]) -> None:
-        low = i + 1
-        high = len(nums) - 1
-        while low < high:
-            sum = nums[i] + nums[low] + nums[high]
-            if sum < 0:
-                low += 1
-            elif sum > 0:
-                high -= 1
-            else:
-                res.append([nums[i], nums[low], nums[high]])
-                low += 1
-                high -= 1
-                while low < high and nums[low] == nums[low-1]:
-                    low += 1
 
 if __name__ == '__main__':
-    assert Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, 0, 1], [-1, -1, 2]]
+    
+    print(Solution().threeSum([-1,0,1,2,-1,-4]))
