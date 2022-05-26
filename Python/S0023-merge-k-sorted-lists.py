2fb9e0f23502d7f94c85c2c049ac5ef65fd74fa4
dennis5943 <dennis5943@gmail.com>
2022-05-27 01:19:15 +0800

    S0023

--- Python/S0023-merge-k-sorted-lists.py
+++ Python/S0023-merge-k-sorted-lists.py
@@ -42,26 +42,55 @@ class ListNode:
         self.next = next
 
 from typing import List
-def from_list(vals: List[int]) -> ListNode:
-    current = None
-    for x in vals[::-1]:
-        current = ListNode(x, current)
-    return current
-
-def to_list(list_node: ListNode) -> List[int]:
-    vals = []
-    while list_node is not None:
-        vals.append(list_node.val)
-        list_node = list_node.next
-    return vals
-
-from typing import List
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        def choose(head,l1,l2):
+            if l1 == None or l2 == None:
+                head.next = l1 if l1 else l2
+                return
+            
+            if l1.val <= l2.val:
+                head.next = ListNode(l1.val)
+                choose(head.next,l1.next,l2)
+            else:
+                head.next = ListNode(l2.val)
+                choose(head.next,l1,l2.next)
+
+        lists = list(filter(lambda l: l != None,lists))
         
-        pass
+        while len(lists) >= 2:
+            tnode = ListNode()
+            choose(tnode,lists[0],lists[1])
+            lists.remove(lists[0])
+            lists.remove(lists[0])
+            lists.append(tnode.next)
+        if not any(lists):
+            return None
+        return lists.pop()
 
 
 if __name__ == '__main__':
-    assert Solution().mergeKLists(0) == 0
 
+    def tolist(listOfnums):
+        def array2list(nums):
+            dummyhead = ListNode(0,None)
+
+            ptr=dummyhead
+            for n in nums:
+                ptr.next = ListNode(n,None)
+                ptr = ptr.next
+            return dummyhead.next
+
+        return [array2list(nums) for nums in listOfnums]
+
+    def lst2array(head):
+        lst=[]
+        n = head
+        while n:
+            lst.append(n.val)
+            n = n.next
+        return lst
+
+    assert lst2array(Solution().mergeKLists(tolist([[1,4,5],[1,3,4],[2,6],[]]))) == [1,1,2,3,4,4,5,6]
+    assert lst2array(Solution().mergeKLists(tolist( []))) == []
+    assert lst2array(Solution().mergeKLists(tolist([[]]))) == []
