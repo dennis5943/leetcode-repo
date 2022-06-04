1b147190fde4ca069f8f07652edacaacd7e6a0ba
dennis5943 <dennis5943@gmail.com>
2022-06-05 01:40:33 +0800

    S0076 (224 / 266 test cases passed.)

--- Python/S0076-minimum-window-substring.py
+++ Python/S0076-minimum-window-substring.py
@@ -12,12 +12,56 @@ Note:
 
 
 """
+from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        pass
+        tc = Counter(t)
+        sc = Counter(s)
+        for k in [kk for kk in sc.keys() if kk not in tc]:
+            del sc[k]
+
+        if any( 1 for k in tc.keys() if tc[k] > sc[k]):
+            return ""
+
+        l = 0
+        r = len(s) - 1
+
+        minStr = lambda s1,s2: s1 if len(s1) <= len(s2) else s2
+        def dfs(ll,rr):
+            while s[ll] not in tc:
+                ll += 1
+
+            while s[rr] not in tc:
+                rr -= 1
+
+            tstr = s[ll:rr + 1]
+            llch = s[ll]
+            rrch = s[rr]
+
+            if sc[llch] > tc[llch]:
+                sc[s[ll]] -= 1
+                lshift = dfs(ll+1,rr)
+                sc[s[ll]] += 1
+                if lshift:
+                    tstr = minStr(lshift,tstr)
+            
+            if sc[rrch] > tc[rrch]:
+                sc[s[rr]] -= 1
+                rshift = dfs(ll,rr - 1)
+                sc[s[rr]] += 1
+                if rshift:
+                    tstr = minStr(rshift,tstr)    
+
+            return tstr
+        substr = dfs(l,r)
+        return substr
 
 
 if __name__ == '__main__':
-    assert Solution().minWindow(0) == 0
+    assert Solution().minWindow("wegdtzwabazduwwdysdetrrctotpcepalxdewzezbfewbabbseinxbqqplitpxtcwwhuyntbtzxwzyaufihclztckdwccpeyonumbpnuonsnnsjscrvpsqsftohvfnvtbphcgxyumqjzltspmphefzjypsvugqqjhzlnylhkdqmolggxvneaopadivzqnpzurmhpxqcaiqruwztroxtcnvhxqgndyozpcigzykbiaucyvwrjvknifufxducbkbsmlanllpunlyohwfsssiazeixhebipfcdqdrcqiwftutcrbxjthlulvttcvdtaiwqlnsdvqkrngvghupcbcwnaqiclnvnvtfihylcqwvderjllannflchdklqxidvbjdijrnbpkftbqgpttcagghkqucpcgmfrqqajdbynitrbzgwukyaqhmibpzfxmkoeaqnftnvegohfudbgbbyiqglhhqevcszdkokdbhjjvqqrvrxyvvgldtuljygmsircydhalrlgjeyfvxdstmfyhzjrxsfpcytabdcmwqvhuvmpssingpmnpvgmpletjzunewbamwiirwymqizwxlmojsbaehupiocnmenbcxjwujimthjtvvhenkettylcoppdveeycpuybekulvpgqzmgjrbdrmficwlxarxegrejvrejmvrfuenexojqdqyfmjeoacvjvzsrqycfuvmozzuypfpsvnzjxeazgvibubunzyuvugmvhguyojrlysvxwxxesfioiebidxdzfpumyon","ozgzyywxvtublcl") == "fsrvmrnczjzjevkdvroiluthhpqtff"
+    assert Solution().minWindow("cgklivwehljxrdzpfdqsapogwvjtvbzahjnsejwnuhmomlfsrvmrnczjzjevkdvroiluthhpqtffhlzyglrvorgnalk","mqfff") == "fsrvmrnczjzjevkdvroiluthhpqtff"
+    assert Solution().minWindow("ab","a") == "a"
+    assert Solution().minWindow("JJDDDADOBECODEBANC","ABC") == "BANC"
+    assert Solution().minWindow("a","a") == "a"
+    assert Solution().minWindow("a","aa") == ""
 
