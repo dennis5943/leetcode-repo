f9b54c96f9f9426e3eb2b4ef2bc351e56711175f
Dennis <dennis5943@gmail.com>
2022-06-24 11:55:54 +0800

    S00101

--- Python/S0101-symmetric-tree.py
+++ Python/S0101-symmetric-tree.py
@@ -24,7 +24,7 @@ But the following [1,2,2,null,3,null,3] is not:
 Follow up: Solve it both recursively and iteratively.
 
 """
-
+from pickle import LIST
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -35,9 +35,32 @@ class TreeNode:
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         
-        pass
+        def CheckSame(left,right):
+            if left and right:
+                return CheckSame(left.left,right.right) and CheckSame(left.right,right.left) if left.val == right.val else False
+            return not left and not right
 
+        return CheckSame(root.left,root.right) if root else False        
+
+def lst2tree(list: LIST) -> TreeNode:
+
+    r = TreeNode(val = list[0])
+
+    def dfs(node,idx):
+        leftIdx = (idx + 1) * 2 - 1
+        rightIdx = (idx + 1) * 2
+
+        if leftIdx < len(list) and list[leftIdx]:
+            node.left = TreeNode(val = list[leftIdx])
+            dfs(node.left,leftIdx)
+        
+        if rightIdx < len(list) and list[rightIdx]:
+            node.right = TreeNode(val = list[rightIdx])
+            dfs(node.right,rightIdx)
+    dfs(r,0)
+    return r
 
 if __name__ == '__main__':
-    assert Solution().isSymmetric(0) == 0
+    assert Solution().isSymmetric(lst2tree([1,2,2,3,4,4,3])) == True
+    assert Solution().isSymmetric(lst2tree([1,2,2,None,3,None,3])) == False
 
