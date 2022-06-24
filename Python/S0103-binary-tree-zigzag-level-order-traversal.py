d2efb46c5497300dc1103a7bc00171356627eaa4
dennis5943 <dennis5943@gmail.com>
2022-06-24 14:29:24 +0800

    S0103

--- Python/S0103-binary-tree-zigzag-level-order-traversal.py
+++ Python/S0103-binary-tree-zigzag-level-order-traversal.py
@@ -29,12 +29,47 @@ class TreeNode:
         self.right = right
 
 from typing import List
+from pickle import LIST
+
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        
-        pass
+        ans = []
 
+        def dfs(node,lv):
+            if node:
+                if lv == len(ans):
+                    ans.append([])
+                
+                if lv%2 == 0:
+                    ans[lv].append(node.val)
+                else:
+                    ans[lv] = [node.val] + ans[lv]
+                dfs(node.left,lv +1)
+                dfs(node.right,lv +1)
+
+            pass
+        dfs(root,0)
+        return ans
+
+def lst2tree(list: LIST) -> TreeNode:
+    if not list:
+        return None
+    r = TreeNode(val = list[0])
+
+    def dfs(node,idx):
+        leftIdx = (idx + 1) * 2 - 1
+        rightIdx = (idx + 1) * 2
+
+        if leftIdx < len(list) and list[leftIdx]:
+            node.left = TreeNode(val = list[leftIdx])
+            dfs(node.left,leftIdx)
+        
+        if rightIdx < len(list) and list[rightIdx]:
+            node.right = TreeNode(val = list[rightIdx])
+            dfs(node.right,rightIdx)
+    dfs(r,0)
+    return r
 
 if __name__ == '__main__':
-    assert Solution().zigzagLevelOrder(0) == 0
+    assert Solution().zigzagLevelOrder(lst2tree([3,9,20,None,None,15,7])) == [[3],[20,9],[15,7]]
 
