5d5745a93f0328b5c4640c1b441f31007f4e50b3
Dennis <dennis5943@gmail.com>
2022-06-27 14:47:41 +0800

    S0108

--- Python/S0108-convert-sorted-array-to-binary-search-tree.py
+++ Python/S0108-convert-sorted-array-to-binary-search-tree.py
@@ -28,10 +28,47 @@ class TreeNode:
 from typing import List
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        
-        pass
+        def solve(left, right):
+            if left > right: return None
+            m = (left + right)//2
+            return TreeNode(nums[m], solve(left,m-1), solve(m+1,right))
+        return solve(0, len(nums)-1)
 
+from pickle import LIST
+def lst2tree(list: LIST) -> TreeNode:
+    if not list:
+        return None
+    r = TreeNode(val = list[0])
+
+    def dfs(node,idx):
+        leftIdx = (idx + 1) * 2 - 1
+        rightIdx = (idx + 1) * 2
+
+        if leftIdx < len(list) and list[leftIdx]:
+            node.left = TreeNode(val = list[leftIdx])
+            dfs(node.left,leftIdx)
+        
+        if rightIdx < len(list) and list[rightIdx]:
+            node.right = TreeNode(val = list[rightIdx])
+            dfs(node.right,rightIdx)
+    dfs(r,0)
+    return r
+
+def validation(root:TreeNode) -> bool:
+    def dfs(node):
+        if not node:
+            return True,0
+        lres,lh = dfs(node.left)
+        if not lres:
+            return False,lh
+        
+        rres,rh = dfs(node.right)
+        if not rres:
+            return False,rh
+        return abs(lh - rh) <= 1,1 + max(lh,rh)
+    return dfs(root)[0]
 
 if __name__ == '__main__':
-    assert Solution().sortedArrayToBST(0) == 0
+    assert validation(Solution().sortedArrayToBST([-10,-3,0,5,9]))
+    assert validation(Solution().sortedArrayToBST([1,3]))
 
