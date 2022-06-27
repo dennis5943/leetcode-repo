6c05c88f22cc81b7b60c413c8675a73c30af70a9
Dennis <dennis5943@gmail.com>
2022-06-27 15:19:31 +0800

    S0118

--- Python/S0118-pascals-triangle.py
+++ Python/S0118-pascals-triangle.py
@@ -20,10 +20,24 @@ Output:
 from typing import List
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        res = []
         
-        pass
+        def dfs(lv,tgtlv):
+            if lv == tgtlv:
+                return
+            elif lv < 2:
+                res.append([1] * (lv + 1))            
+            else:                
+                nums = [1] + [res[lv-1][i] + res[lv-1][i + 1] for i in range(lv-1)] + [1]
+                res.append(nums)
 
+            dfs(lv + 1,numRows)
+            pass
+
+        dfs(0,numRows)
+        return res
 
 if __name__ == '__main__':
-    assert Solution().generate(0) == 0
+    assert Solution().generate(5) == [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+    assert Solution().generate(1) == [[1]]
 
